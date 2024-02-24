708d124cc0b5372da9203558fec986ea4a82adee
Adejare <adejare.arasheed@gmail.com>
2024-02-24 15:06:07 +0100

    AirBnB_v2 Clone

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,11 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
 from models.base_model import BaseModel
+from models.amenity import Amenity
 from models.city import City
-from models.state import State
+from models.place import Place
 from models.review import Review
+from models.state import State
 from models.user import User
 
 
