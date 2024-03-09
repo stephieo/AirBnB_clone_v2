267bc3f1efd36217ac8600f3aaa67ada6aac0ed5
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-09 20:17:36 +0100

    fix: change destination line in sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,23 +2,25 @@
 # script to set up web servers for deployment of AirBnB_clone webstatic
 
 # check for NGINX and install if not found
-#if (( "$ which nginx | grep - nginx > /dev/null"  == 0 ));then
-#	echo "Already installed";
-#else
-#	echo "Begin installation"
+if (( "$ which nginx | grep - nginx > /dev/null"  == 0 ));then
+	echo "Already installed";
+else
+	echo "Begin installation"
 	sudo apt-get update
 	sudo apt-get install -y nginx
 	sudo ufw allow  'Nginx HTTP'
 
 	echo "Hello World!" | sudo tee /var/www/html/index.debian.html
 	sudo service nginx restart
-#fi
+fi
 
 # create folders && subfolders
 mkdir -p "/data/web_static/shared." 
 mkdir -p "/data/web_static/releases/test/"
-#mkdir -p "web_static" && cd "web_static"
-#mkdir -p "releases" "shared" && cd "releases"
+
+# mkdir -p "web_static" && cd "web_static"
+# mkdir -p "releases" "shared" && cd "releases"
+
 cd /data/web_static/releases/test/data/web_static/releases/ && touch index.html
 
 # populate test file
@@ -48,7 +50,7 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # INSERT NGINX CONFIG WITH ALIAS HERE
-sudo sed -i '/last line/ a    \\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}'
+sudo sed -i '/server_name _;/ a    \\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}'
 
 # restart NGINX
 sudo service nginx restart
