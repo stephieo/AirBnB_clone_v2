37820baf4e666ef51f6c2f74ac4515b55055e769
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-24 20:44:56 +0100

    feat: flask app with new route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script to start a flask application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """return function for this route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """return function for this route"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
