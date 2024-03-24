e70ce5ef36a875adfcc420de168c417bd21b28a6
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-24 20:13:44 +0100

    feat: set up simple Flask application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" this script starts the flask application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/",)
+def hello():
+    """return function for this route"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
