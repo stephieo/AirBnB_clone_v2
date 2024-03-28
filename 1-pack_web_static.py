e10d1e1f070ba70ef772af5cdde92031c7c45686
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-28 16:04:49 +0100

    feat: fabric script to compress files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+from invoke import task, run
+from fabric import Connection
+from datetime import datetime
+
+@task
+def do_pack(c):
+    """ """
+    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = f"web_static_{current_date}.tgz"
+    local("mkdir -p versions")
+    test = (f"tar -czvf versions/{archive_name} web_static/")
+    if test.succeeded:
+        return f"versions/{archive_name}"
+    
+if __name__ == "__name__":
+    do_pack()
